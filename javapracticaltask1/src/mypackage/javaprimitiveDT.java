b0d565c56f9febc79c6929e5985e07534a2f9cbd
saikumar841 <chintasaikumar222@gmail.com>
2025-07-26 11:10:58 +0530

    javaprimitiveDT.java

--- javapracticaltask1/src/mypackage/javaprimitiveDT.java
+++ javapracticaltask1/src/mypackage/javaprimitiveDT.java
@@ -3,23 +3,14 @@ package mypackage;
 public class javaprimitiveDT {
 
 	public static void main(String[] args) {
-		
-		        // Integer types
 		        byte b = 10;
 		        short s = 100;
 		        int i = 1000;
-		        long l = 10000L;
-
-		        // Floating point types
+		        long l = 10000L
 		        float f = 10.5f;
 		        double d = 20.99;
-
-		        // Character type
 		        char c = 'A';
-
-		        // Boolean type
 		        boolean bool = true;
-
 		        System.out.println("byte value: " + b);
 		        System.out.println("short value: " + s);
 		        System.out.println("int value: " + i);
